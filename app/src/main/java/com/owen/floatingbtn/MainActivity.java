6d74b79b13bc1e221ca460d97cb0197a6c3aa1e2
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-03-19 09:02:16 -0400

    Update MainActivity.java

--- app/src/main/java/com/owen/floatingbtn/MainActivity.java
+++ app/src/main/java/com/owen/floatingbtn/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    FloatingActionButton floatingbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        floatingbtn = (FloatingActionButton)findViewByid(R.id.checkMoreinfo_floatin));
     }
 }
